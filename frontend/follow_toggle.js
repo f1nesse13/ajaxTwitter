7f779da49dced7c4621ab5738ad699875bdd722d
Joe Pell <jpell3130@gmail.com>
2019-06-21 10:46:04 -0400

    made intentions more clear by using followed / unfollowed instead of true false

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -2,13 +2,14 @@ class FollowToggle {
   constructor($el) {
     this.$el = $el;
     this.userId = $(this.$el).attr('data-user-id');
-    this.followState = $(this.$el).attr('data-initial-follow-state');
+    this.followState =
+      $(this.$el).attr('data-initial-follow-state') === 'true' ? 'followed' : 'unfollowed';
     this.render();
     $(this.$el).on('click', this.handleClick.bind(this));
   }
 
   render() {
-    if (this.followState === 'true') {
+    if (this.followState === 'followed') {
       this.$el.innerHTML = 'Unfollow!';
     } else {
       this.$el.innerHTML = 'Follow!';
@@ -17,13 +18,13 @@ class FollowToggle {
 
   handleClick(e) {
     e.preventDefault();
-    const reqMethod = this.followState === 'true' ? 'DELETE' : 'POST';
+    const reqMethod = this.followState === 'followed' ? 'DELETE' : 'POST';
     $.ajax({
       method: reqMethod,
       url: `${this.userId}/follow`,
       dataType: 'json',
       success: function(data) {
-        this.followState = reqMethod === 'DELETE' ? 'false' : 'true';
+        this.followState = reqMethod === 'DELETE' ? 'unfollowed' : 'followed';
         this.render();
       }.bind(this)
     });

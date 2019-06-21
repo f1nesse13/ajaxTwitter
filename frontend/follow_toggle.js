ac9eb79a30b96325c642532cbf137ffaac3dbe3b
Joe Pell <jpell3130@gmail.com>
2019-06-21 10:39:55 -0400

    added followtoggle that makes a ajax call to change the status and button text when clicked

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -1,9 +1,32 @@
 class FollowToggle {
   constructor($el) {
-    this.$el = $($el);
-    this.userId = this.$el.attr('data-user-id');
-    this.followState = this.$el.attr('data-initial-follow-state');
+    this.$el = $el;
+    this.userId = $(this.$el).attr('data-user-id');
+    this.followState = $(this.$el).attr('data-initial-follow-state');
+    this.render();
+    $(this.$el).on('click', this.handleClick.bind(this));
+  }
+
+  render() {
+    if (this.followState === 'true') {
+      this.$el.innerHTML = 'Unfollow!';
+    } else {
+      this.$el.innerHTML = 'Follow!';
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const reqMethod = this.followState === 'true' ? 'DELETE' : 'POST';
+    $.ajax({
+      method: reqMethod,
+      url: `${this.userId}/follow`,
+      dataType: 'json',
+      success: function(data) {
+        this.followState = reqMethod === 'DELETE' ? 'false' : 'true';
+        this.render();
+      }.bind(this)
+    });
   }
 }
-
 module.exports = FollowToggle;

537d63ac49f5cd38137a522bd4cb47de965d1a89
Joe Pell <jpell3130@gmail.com>
2019-06-20 20:02:53 -0400

    initial commit // created FollowToggle class required in twitter.js // follows/_form edited to recieve async updates

---
+++ frontend/twitter.js
@@ -0,0 +1,8 @@
+const FollowToggle = require('follow_toggle.js');
+
+$(() => {
+  // creates a FollowToggle instance for each .follow-toggle button
+  $('.follow-toggle').each(function(i, ele) {
+    new FollowToggle(ele);
+  });
+});

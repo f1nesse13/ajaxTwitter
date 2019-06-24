0521b7f9a3de299d2eca7025c704195b7f730e85
Joe Pell <jpell3130@gmail.com>
2019-06-24 19:26:56 -0400

    added comments to tweet_compose.js

--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -29,17 +29,20 @@ class TweetCompose {
         this.handleSuccess(data);
       })
       .fail(err => {
+        // disables the disabled prop so a user can fix any mistakes - does not clear input so user will not lose progress - logs the error for dev purposes
         this.$el.find(':input').prop('disabled', false);
         console.log(err);
       });
   }
 
   clearInput() {
+    // clears input fields
     this.$el.find('textarea').val('');
     this.$el.find('select').val('');
   }
 
   handleSuccess(data) {
+    // disables the disabled property clears our input fields and prepends the new li to the list of tweets
     this.$el.find(':input').prop('disabled', false);
     this.clearInput();
     const $ul = $(this.$el.attr('data-tweets-ul'));
@@ -48,11 +51,15 @@ class TweetCompose {
   }
 
   updateCounter() {
+    // grab textarea and the counter
     const $textarea = this.$el.find('textarea');
     const $counter = this.$el.find('.chars-left');
+    // counts characters inside textarea
     let charsLeft = $($textarea).val().length;
+    // resets the character count for each input event
     $($counter).text('');
     $($counter).text(`${charsLeft}/140`);
+    // if the user hits 140 or more characters we will keep setting the first 140 chars using substring and insert them essentially stopping a user from adding more
     if (charsLeft >= 140) {
       $($textarea).val(
         $($textarea)
@@ -64,13 +71,17 @@ class TweetCompose {
 
   newUserSelect(e) {
     e.preventDefault();
+    // link used to remove this mention field
     const $removeMention = $('<a>')
       .attr('href', '#')
       .text('--Remove mention--')
       .addClass('remove-mention');
+    // parent div that contains all elements of a added mention field
     const $mentionDiv = $('<div>');
     const $select = $('<select>').attr('name', 'tweet[mentioned_user_ids][]');
+    // parent of our container - holds all divs with seperate selects for mentions along with link to remove
     const $mentionList = $(`.mentions`);
+    // generates a new select field with options to choose each user - users stored in window.users global variable
     window.users.forEach(user => {
       $($mentionDiv).append(
         $select.append(
@@ -80,17 +91,21 @@ class TweetCompose {
         )
       );
     });
-    $('.feed-form').css('height', '+=35px');
+    // adds 53px of height to account for the 53px container div (<select> with <a> tag to remove the select)
+    $('.feed-form').css('height', '+=53px');
     $mentionDiv.append($removeMention);
     $mentionDiv.appendTo($mentionList);
   }
 
   removeMentionedUser(e) {
+    // removes a div containing our generated select field and removal link
     e.preventDefault();
+    // listens on parent .mentions and targets .remove-mention's parent element which is the generated div
     $(e.currentTarget)
       .parent()
       .remove();
-    $('.feed-form').css('height', '-=35px');
+    // reduces the size of the form that is added when we add a new mention field
+    $('.feed-form').css('height', '-=53px');
   }
 }
 

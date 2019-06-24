87873da8c1dce0b41414af7da191f04595334b45
Joe Pell <jpell3130@gmail.com>
2019-06-24 19:11:12 -0400

    implemented ability to add and remove mentions on form for new tweets

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -199,20 +199,29 @@ module.exports = FollowToggle;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
+/* eslint-disable class-methods-use-this */
 const APIUtil = __webpack_require__(/*! ./api_util.js */ "./frontend/api_util.js");
 
 class TweetCompose {
   constructor($el) {
     this.$el = $($el);
     this.$textarea = $(this.$el.find('textarea'));
+    this.$addMentionButton = this.$el.find('.mentions .add-mention');
     this.$el.on('submit', this.submit.bind(this));
     this.$textarea.on('input', this.updateCounter.bind(this));
+    this.$addMentionButton.on('click', this.newUserSelect.bind(this));
+    $(this.$el.find('.mentions')).on(
+      'click',
+      '.remove-mention',
+      this.removeMentionedUser.bind(this)
+    );
     this.updateCounter();
   }
 
   submit(e) {
     e.preventDefault();
     const formData = this.$el.serializeJSON();
+    console.log(formData);
     // disables all inputs while we try to add to DB
     this.$el.find(':input').prop('disabled', true);
     // sends form data to ajax call
@@ -222,6 +231,7 @@ class TweetCompose {
       })
       .fail(err => {
         this.$el.find(':input').prop('disabled', false);
+        console.log(err);
       });
   }
 
@@ -251,6 +261,37 @@ class TweetCompose {
           .substring(0, 139)
       );
     }
+  }
+
+  newUserSelect(e) {
+    e.preventDefault();
+    const $removeMention = $('<a>')
+      .attr('href', '#')
+      .text('--Remove mention--')
+      .addClass('remove-mention');
+    const $mentionDiv = $('<div>');
+    const $select = $('<select>').attr('name', 'tweet[mentioned_user_ids][]');
+    const $mentionList = $(`.mentions`);
+    window.users.forEach(user => {
+      $($mentionDiv).append(
+        $select.append(
+          $('<option>')
+            .attr('value', user.id)
+            .html(user.username)
+        )
+      );
+    });
+    $('.feed-form').css('height', '+=35px');
+    $mentionDiv.append($removeMention);
+    $mentionDiv.appendTo($mentionList);
+  }
+
+  removeMentionedUser(e) {
+    e.preventDefault();
+    $(e.currentTarget)
+      .parent()
+      .remove();
+    $('.feed-form').css('height', '-=35px');
   }
 }
 
--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -1,17 +1,26 @@
+/* eslint-disable class-methods-use-this */
 const APIUtil = require('./api_util.js');
 
 class TweetCompose {
   constructor($el) {
     this.$el = $($el);
     this.$textarea = $(this.$el.find('textarea'));
+    this.$addMentionButton = this.$el.find('.mentions .add-mention');
     this.$el.on('submit', this.submit.bind(this));
     this.$textarea.on('input', this.updateCounter.bind(this));
+    this.$addMentionButton.on('click', this.newUserSelect.bind(this));
+    $(this.$el.find('.mentions')).on(
+      'click',
+      '.remove-mention',
+      this.removeMentionedUser.bind(this)
+    );
     this.updateCounter();
   }
 
   submit(e) {
     e.preventDefault();
     const formData = this.$el.serializeJSON();
+    console.log(formData);
     // disables all inputs while we try to add to DB
     this.$el.find(':input').prop('disabled', true);
     // sends form data to ajax call
@@ -21,6 +30,7 @@ class TweetCompose {
       })
       .fail(err => {
         this.$el.find(':input').prop('disabled', false);
+        console.log(err);
       });
   }
 
@@ -50,6 +60,37 @@ class TweetCompose {
           .substring(0, 139)
       );
     }
+  }
+
+  newUserSelect(e) {
+    e.preventDefault();
+    const $removeMention = $('<a>')
+      .attr('href', '#')
+      .text('--Remove mention--')
+      .addClass('remove-mention');
+    const $mentionDiv = $('<div>');
+    const $select = $('<select>').attr('name', 'tweet[mentioned_user_ids][]');
+    const $mentionList = $(`.mentions`);
+    window.users.forEach(user => {
+      $($mentionDiv).append(
+        $select.append(
+          $('<option>')
+            .attr('value', user.id)
+            .html(user.username)
+        )
+      );
+    });
+    $('.feed-form').css('height', '+=35px');
+    $mentionDiv.append($removeMention);
+    $mentionDiv.appendTo($mentionList);
+  }
+
+  removeMentionedUser(e) {
+    e.preventDefault();
+    $(e.currentTarget)
+      .parent()
+      .remove();
+    $('.feed-form').css('height', '-=35px');
   }
 }
 
